ef8c5fd41708d15e356a0f09bb92ea9975563fe2
Manuel Schmidt <manu.schmidt.14@gmail.com>
2021-05-03 21:47:51 +0200

    Started working on Agent

--- agent.py
+++ agent.py
@@ -3,3 +3,37 @@ This file will hold the agent class.
 The agent class implements the deep Q-Network algorithm explained here:
 https://storage.googleapis.com/deepmind-media/dqn/DQNNaturePaper.pdf
 """
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+from model import QNetwork
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Agent():
+    """ This class represents the reinforcement learning agent """
+
+    def __init__(self, state_size: int, action_size: int, hidden_sizes: [int] = (64, 64),
+                 gamma: float = 0.99, lr: float = 5e-3, tau: float = 1e-3,
+                 eps_start: float = 1.0, eps_dec: float = .9995, eps_min: float = 0.01,
+                 seed: int = int(random.random() * 100)):
+        """
+        Initializes the agent
+        :param state_size: dimensions of a state
+        :param action_size: dimension of a action
+        :param hidden_sizes: array containing the size for each hidden layer of the deep Q network
+        :param gamma: discount factor for learning
+        :param lr: learning rate
+        :param tau: #todo don't know what this does, something with soft update?
+        :param eps_start: epsilon start value
+        :param eps_dec: epsilon decay per episode
+        :param eps_min: minimum value for epsilon (never stop exploring)
+        :param seed: seed to get comparable model runs
+        """
+        pass
--- model.py
+++ model.py
@@ -12,7 +12,30 @@ class QNetwork(nn.Module):
     def __init__(self, input_size: int, hidden_sizes: [int], output_size: int) -> None:
         """
         Initializes the layers of the network
-        :param input_size:
-        :param hidden_sizes:
-        :param output_size:
+        :param input_size: size of the first layer
+        :param hidden_sizes: array containing the size for each hidden layer
+        :param output_size: size of the output layer
         """
+        super().__init__()
+        self.layers = nn.ModuleList()
+
+        # add input layer
+        self.layers.append(nn.Linear(in_features=input_size, out_features=hidden_sizes[0]))
+
+        # add hidden layers
+        for i in range(len(hidden_sizes) - 1):
+            self.layers.append(nn.Linear(in_features=hidden_sizes[i], out_features=hidden_sizes[i + i]))
+
+        # add output layer
+        self.layers.append(nn.Linear(in_features=hidden_sizes[-1], out_features=output_size))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Do a forward pass through the network
+        :param x: data to pass through the network
+        :return:
+        """
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+        out = F.softmax(self.layers[-1](x))
+        return out
